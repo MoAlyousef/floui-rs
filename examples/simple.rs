9c1a7bfa44f42a244af061ae4a406de1cc55d2dd
MoAlyousef <may642_2000@hotmail.com>
2022-07-10 16:07:41 +0300

    update example

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,5 +1,7 @@
 use floui::*;
+use std::cell::RefCell;
 use std::os::raw::c_void;
+use std::rc::Rc;
 
 #[no_mangle]
 extern "C" fn floui_handle_events(arg1: *mut c_void) {
@@ -9,15 +11,31 @@ extern "C" fn floui_handle_events(arg1: *mut c_void) {
 #[no_mangle]
 extern "C" fn floui_main(arg1: *mut c_void, arg2: *mut c_void, arg3: *mut c_void) -> *mut c_void {
     let fvc = ViewController::new(arg1, arg2, arg3);
-    MainView::new(&fvc, &[
-        &Button::new("Increment").action(|_| { 
-            log("Increment clicked");
-            let t: Text = from_id("mytext").unwrap();
-            t.text("works");
-        }),
-        &Text::new("0").id("mytext"),
-        &Button::new("Decrement").action(|_| log("Increment clicked"))
-    ]).inner() as _
+    let count = Rc::from(RefCell::from(0));
+    MainView::new(
+        &fvc,
+        &[
+            &Button::new("Increment").action({
+                let count = count.clone();
+                move |_| {
+                    log("Increment clicked");
+                    let mut c = count.borrow_mut();
+                    *c += 1;
+                    let t: Text = from_id("mytext").unwrap();
+                    t.text(&format!("{}", c));
+                }
+            }),
+            &Text::new("0").id("mytext"),
+            &Button::new("Decrement").action(move |_| {
+                log("Increment clicked");
+                let mut c = count.borrow_mut();
+                *c -= 1;
+                let t: Text = from_id("mytext").unwrap();
+                t.text(&format!("{}", c));
+            }),
+        ],
+    )
+    .inner() as _
 }
 
 fn main() {}
